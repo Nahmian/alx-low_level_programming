31adc42b1f742f98ca2230d10becfda9fa6d7eb1
Nahmian <a.nouhayla@gmail.com>
2023-10-11 08:09:31 -0700

    okay

--- 0x0F-function_pointers/0-main.c
+++ 0x0F-function_pointers/0-main.c
@@ -20,15 +20,15 @@ void print_name_as_is(char *name)
  */
 void print_name_uppercase(char *name)
 {
-    unsigned int i;
+	unsigned int i;
 
-    printf("Hello, my uppercase name is ");
-    i = 0;
-    while (name[i])
-    {
-        if (name[i] >= 'a' && name[i] <= 'z')
-        {
-            putchar(name[i] + 'A' - 'a');
+	printf("Hello, my uppercase name is ");
+	i = 0;
+	while (name[i])
+	{
+	if (name[i] >= 'a' && name[i] <= 'z')
+	 {
+	putchar(name[i] + 'A' - 'a');
         }
         else
         {

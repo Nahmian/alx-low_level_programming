c0a7480ea24e1324727f226090d9a34cfc3a13d7
Nahmian <a.nouhayla@gmail.com>
2023-10-20 14:53:04 -0700

    lol

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -2,9 +2,9 @@
 #include "lists.h"
 
 /**
-* free_list - frees a linked list
-* @head: list_t list to be freed
-*/
+ * free_list - frees a linked list
+ * @head: list_t list to be freed
+ */
 void free_list(list_t *head)
 {
 list_t *temp;

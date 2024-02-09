61ce0d7b2b8421ff0b6ed9a118fdec494744ef85
Nahmian <a.nouhayla@gmail.com>
2024-02-09 01:36:55 -0800

    khdem

--- 0x0F-function_pointers/0-main.c
+++ 0x0F-function_pointers/0-main.c
@@ -1,23 +1,23 @@
 #include <stdio.h>
 #include "function_pointers.h"
 
-/**
- * print_name_as_is - prints a name as is
- * @name: name of the person
- *
- * Return: Nothing.
- */
+	/**
+	* print_name_as_is - prints a name as is
+	* @name: name of the person
+	*
+	* Return: Nothing.
+	*/
 void print_name_as_is(char *name)
 {
-    printf("Hello, my name is %s\n", name);
+	printf("Hello, my name is %s\n", name);
 }
 
-/**
- * print_name_uppercase - print a name in uppercase
- * @name: name of the person
- *
- * Return: Nothing.
- */
+	/**
+	* print_name_uppercase - print a name in uppercase
+	* @name: name of the person
+	*
+	* Return: Nothing.
+	*/
 void print_name_uppercase(char *name)
 {
 	unsigned int i;
@@ -27,26 +27,25 @@ void print_name_uppercase(char *name)
 	while (name[i])
 	{
 	if (name[i] >= 'a' && name[i] <= 'z')
-	 {
+	{
 	putchar(name[i] + 'A' - 'a');
-        }
-        else
-        {
-            putchar(name[i]);
-        }
-        i++;
-    }
 }
-
+	else
+        {
+	putchar(name[i]);
+        }
+	i++;
+	}
+}
 /**
- * main - check the code
- *
- * Return: Always 0.
- */
+	* main - check the code
+	*
+	* Return: Always 0.
+	*/
 int main(void)
 {
-    print_name("Bob", print_name_as_is);
-    print_name("Bob Dylan", print_name_uppercase);
-    printf("\n");
-    return (0);
+	print_name("Bob", print_name_as_is);
+	print_name("Bob Dylan", print_name_uppercase);
+	printf("\n");
+	return (0);
 }
